81028c2aea73fbc00d9f2a8d4e15916eaea8b30d
Chenyang (Raphael) Du <raphael_du@yeah.net>
2024-12-06 19:59:26 -0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_27_010537) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_235132) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_010537) do
     t.text "description"
     t.string "image"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
